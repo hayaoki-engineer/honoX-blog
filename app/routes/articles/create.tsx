3d604b3cf6f351cf6471ddcc4f26b4993aa1bf52
Taisei <taisei.960420@gmail.com>
2024-08-23 08:16:33 +0900

    バリデーションの処理

--- app/routes/articles/create.tsx
+++ app/routes/articles/create.tsx
@@ -1,63 +1,57 @@
 import { css } from "hono/css";
 import { createRoute } from "honox/factory";
 import type { FC } from "hono/jsx";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { buttonClass, errorClass, formClass, inputClass, labelClass, textareaClass, titleClass } from "../../style.css";
+ 
 
-const titleClass = css`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-`;
+const Article = z.object({
+  title: z.string().min(1).max(255),
+  content: z.string().min(1),
+});
 
-const formClass = css`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
+type Data = {
+  title: {
+    value: string;
+    error: string[] | undefined;
+  };
+  content: {
+    value: string;
+    error: string[] | undefined;
+  };
+};
 
-const labelClass = css`
-  display: flex;
-  flex-direction: column;
-  font-weight: bold;
-  gap: 0.5rem;
-`;
+type Props = {
+  data?: Data;
+}
 
-const inputClass = css`
-  width: 100%;
-  padding: 0.5rem 0.25em;
-  border-radius: 3px;
-  border: 2px solid #ddd;
-`;
-
-const textareaClass = css`
-  width: 100%;
-  border: 2px solid #ddd;
-  border-radius: 3px;
-  padding: 0.5rem;
-  min-height: 10rem;
-  resize: vertical;
-`;
-
-const buttonClass = css`
-  padding: 0.5rem;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  cursor: pointer;
-  border-radius: 999px;
-`;
-
-const Page: FC = () => {
+const Page: FC<Props> = ({ data }) => {
   return (
     <div>
       <h1 class={titleClass}>Create an article</h1>
       <form class={formClass} method="post">
         <label class={labelClass}>
           Title
-          <input name="title" class={inputClass} type="text" />
+          <input
+            name="title"
+            class={inputClass}
+            type="text"
+            value={data?.title.value}
+          />
         </label>
+        {data?.title.error &&
+          data.title.error.map((error) => <p class={errorClass}>{error}</p>)}
         <label class={labelClass}>
           Content
-          <textarea name="content" class={textareaClass} />
+          <textarea
+            name="content"
+            class={textareaClass}
+            value={data?.content.value}
+          />
         </label>
+        {data?.content.error &&
+          data.content.error.map((error) => <p class={errorClass}>{error}</p>)}
         <button class={buttonClass} type="submit">
           Create
         </button>
@@ -72,11 +66,32 @@ export default createRoute((c) => {
   });
 })
 
-export const POST = createRoute(async (c) => {
-  const body = await c.req.formData();
+// POSTリクエストの処理
+export const POST = createRoute(
+  zValidator("form", Article, (result, c) => {
+    
+    if (result.success) {
+      // TODO DB に保存
+      console.log(result.data);
+      return c.redirect("/articles")
+    }
 
-  console.log("title:", body.get("title"));
-  console.log("content:", body.get("content"));
+    // バリデーションの結果
+    const { title, content } = result.data;
+    const data: Data = {
+      title: {
+        value: title,
+        error: result.error.flatten().fieldErrors.title,
+      },
+      content: {
+        value: content,
+        error: result.error.flatten().fieldErrors.content,
+      },
+    };
 
-  return c.redirect("/articles");
-})
+    return c.render(<Page data={data} />, {
+      title: "Create an article",
+    });
+
+  })
+);
