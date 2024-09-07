591cffcb292bc7c46008fce4d32309e8c279be5c
Taisei <taisei.960420@gmail.com>
2024-09-07 17:47:40 +0900

    safeParseの処理を追加

--- app/routes/articles/index.tsx
+++ app/routes/articles/index.tsx
@@ -1,8 +1,11 @@
 import { createRoute } from "honox/factory";
 import { getArticles } from "../../lib/db";
-import Page from "../../islands/ArticlesPageContainer";
+import Page, { articleSchema, Article } from "../../islands/ArticlesPageContainer";
 import ArticlesPageContainer from "../../islands/ArticlesPageContainer";
 import { deleteCookie, getCookie } from "hono/cookie";
+import { z } from "zod";
+
+const mySchema = z.string()
 
 export default createRoute(async (c) => {
   const rows = 5;
@@ -11,6 +14,22 @@ export default createRoute(async (c) => {
 
   // サーバーサイドでデータを取得
   const res = await getArticles(rows, page);
+  console.log("resの中身", res.articles);
+
+  const parsedArticles = res.articles
+    .map((article: Article) => {
+      const result = articleSchema.safeParse(article);
+      
+      if (result.success) {
+        return result.data; // id と title のみ
+      } else {
+        console.error(result.error);
+        return null; // エラーハンドリング
+      }
+    })
+    .filter(Boolean); // nullを除外
+  
+  console.log('パース', parsedArticles);
 
   const success = getCookie(c, "success");
   console.log("成功状態", success);
@@ -22,7 +41,7 @@ export default createRoute(async (c) => {
 
   return c.render(
     <ArticlesPageContainer
-      articles={res.articles}
+      articles={parsedArticles}
       hasNext={res.hasNext}
       currentPage={page}
       success={success}
